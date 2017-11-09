131b3cef2bb048f14a6737f04a29db3af30bbb01
txkoutny <txkoutny@kiv.zcu.cz>
2017-11-09 14:33:03 +0100

    implemented pipe

--- factory/src/pipe.cpp
+++ factory/src/pipe.cpp
@@ -1,6 +1,26 @@
 #include "pipe.h"
 
+#include "../../../common/rtl/manufactory.h"
+
+
 HRESULT IfaceCalling create_filter_pipe(glucose::IFilter_Pipe **pipe) {
-	
-	return E_NOTIMPL;
+	return Manufacture_Object<CFilter_Pipe, glucose::IFilter_Pipe>(pipe);
+}
+
+CFilter_Pipe::CFilter_Pipe() {
+	mQueue.set_capacity(mDefault_Capacity);
+}
+
+CFilter_Pipe::~CFilter_Pipe() {
+	//just empty to call all the dctors correctly
+}
+
+HRESULT CFilter_Pipe::send(const glucose::TSensor_Event *event) {
+	mQueue.push(*event); 
+	return S_OK;
+}
+
+HRESULT CFilter_Pipe::receive(glucose::TSensor_Event *event) {
+	mQueue.pop(*event);
+	return S_OK;
 }
--- factory/src/pipe.h
+++ factory/src/pipe.h
@@ -1,6 +1,24 @@
 #pragma once
 
 #include "../../../common/iface/FilterIface.h"
+#include "../../../common/rtl/referencedImpl.h"
+
+#include <tbb/concurrent_queue.h>
+
+
+class CFilter_Pipe :  public glucose::IFilter_Pipe, public virtual refcnt::CReferenced {
+protected:
+	tbb::concurrent_bounded_queue<glucose::TSensor_Event> mQueue;
+	const std::ptrdiff_t mDefault_Capacity = 64;
+public:	
+	CFilter_Pipe();
+	virtual ~CFilter_Pipe();
+
+	virtual HRESULT send(const glucose::TSensor_Event *event);
+	virtual HRESULT receive(glucose::TSensor_Event *event);
+};
+
+
 
 #ifdef _WIN32
 	extern "C" __declspec(dllexport) HRESULT IfaceCalling create_filter_pipe(glucose::IFilter_Pipe **pipe);
