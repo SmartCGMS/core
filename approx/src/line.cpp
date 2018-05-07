54abbdc55f59f6d59d7f2acf988dff94dc5a5f26
Tomáš Koutný <txkoutny@kiv.zcu.cz>
2018-05-07 13:20:44 +0200

    bugfixing

--- approx/src/line.cpp
+++ approx/src/line.cpp
@@ -1,5 +1,6 @@
 #include "line.h"
 
+#include <assert.h>
 
 CLine_Approximator::CLine_Approximator(glucose::WSignal signal, glucose::IApprox_Parameters_Vector* configuration)
 	: mSignal(signal)
@@ -18,7 +19,7 @@ void CLine_Approximator::Update() {
 	{
 		mInputTimes.resize(valCount);
 		mInputLevels.resize(valCount);
-		mSlopes.resize(valCount - 1);
+		mSlopes.resize(valCount);
 	}
 	else // valCount == oldCount, no need to update
 		return;
@@ -29,10 +30,14 @@ void CLine_Approximator::Update() {
 	// calculate slopes
 	for (size_t i = 0; i < valCount - 1; i++)
 		mSlopes[i] = (mInputLevels[i + 1] - mInputLevels[i]) / (mInputTimes[i + 1] - mInputTimes[i]);
+	if (valCount > 1) mSlopes[valCount - 1] = mSlopes[valCount - 2];	//special case that will reduxe GetLevels' complexity
 }
 
-HRESULT IfaceCalling CLine_Approximator::GetLevels(const double* times, double* const levels, const size_t count, size_t derivation_order)
-{
+HRESULT IfaceCalling CLine_Approximator::GetLevels(const double* times, double* const levels, const size_t count, size_t derivation_order) {
+
+	assert((times != nullptr) && (levels != nullptr) && (count > 0));
+	if ((times == nullptr) || (levels == nullptr)) return E_INVALIDARG;
+
 	Update();
 
 	// needs to call Approximate first
@@ -41,43 +46,36 @@ HRESULT IfaceCalling CLine_Approximator::GetLevels(const double* times, double* 
 
 	// size of mSlopes is lower by 1 than mInputTimes/Levels, and we know how to approximate just in this range
 	for (size_t i = 0; i< count; i++) {
-
-		auto iter = std::upper_bound(mInputTimes.begin(), mInputTimes.end(), times[i]);
-		size_t knotIndex = std::distance(mInputTimes.begin(), iter) - 1;
-
-		if (knotIndex < mSlopes.size()) {
-
-			switch (derivation_order) {
-				case glucose::apxNo_Derivation:
-						levels[i] = mSlopes[knotIndex]*(times[i] - mInputTimes[knotIndex]) + mInputLevels[knotIndex];
-						break;
-
-				case glucose::apxFirst_Order_Derivation:
-						levels[i] = mSlopes[knotIndex];
-						break;
-
-				default: levels[i] = 0.0;
-						break;
+		size_t knot_index = std::numeric_limits<size_t>::max();
+		if (times[i] == mInputTimes[0]) knot_index = 0;
+			else if (times[i] == mInputTimes[mInputTimes.size() - 1]) knot_index = mInputTimes.size() - 1;
+			else {
+				std::vector<double>::iterator knot_iter = std::upper_bound(mInputTimes.begin(), mInputTimes.end(), times[i]);	
+				if (knot_iter != mInputTimes.end()) knot_index = std::distance(mInputTimes.begin(), knot_iter) - 1;								
 			}
-		}
 
-		else if (knotIndex == mSlopes.size()) {
-			switch (derivation_order) {
-				case glucose::apxNo_Derivation:
-					levels[i] = mInputLevels[knotIndex];
-					break;
 
-				case glucose::apxFirst_Order_Derivation:
-					levels[i] = mSlopes[knotIndex-1];
-					break;
+		
+		
 
-				default: levels[i] = 0.0;
-					break;
+			if (knot_index != std::numeric_limits<size_t>::max()) {
+
+				switch (derivation_order) {
+					case glucose::apxNo_Derivation:
+						levels[i] = mSlopes[knot_index] * (times[i] - mInputTimes[knot_index]) + mInputLevels[knot_index];
+						break;
+
+					case glucose::apxFirst_Order_Derivation:
+						levels[i] = mSlopes[knot_index];
+						break;
+
+					default: levels[i] = 0.0;
+						break;
+					}
 			}
-		}
 		else
 			levels[i] = std::numeric_limits<double>::quiet_NaN();
 	}
 
-	return S_OK;
+	return count > 0 ? S_OK : S_FALSE;
 }
