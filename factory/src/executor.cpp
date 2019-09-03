c99e14226408cdbccbeca94ca58f0bbd1677cea5
Tomáš Koutný <txkoutny@kiv.zcu.cz>
2019-09-03 14:28:15 +0200

    implementing filter executors

---
+++ factory/src/executor.cpp
@@ -0,0 +1,95 @@
+#include "executor.h"
+
+CFilter_Executor::CFilter_Executor(glucose::IFilter_Executor *consument) : mConsument{consument} {
+	mQueue.set_capacity(mDefault_Capacity);
+}
+
+
+HRESULT IfaceCalling CFilter_Executor::receive(glucose::IDevice_Event **event) {
+	if (mShutting_Down_Receive)
+		return S_FALSE;
+
+	try {
+		mQueue.pop(*event);
+
+		glucose::TDevice_Event *raw_event;
+		if ((*event)->Raw(&raw_event) == S_OK)	//should be OK since we call it in ::send
+			mShutting_Down_Receive = raw_event->event_code == glucose::NDevice_Event_Code::Shut_Down;
+	}
+
+	catch (tbb::user_abort &) {
+		mShutting_Down_Receive = true;
+		return S_FALSE;
+	}
+	return S_OK;
+
+}
+
+HRESULT IfaceCalling CFilter_Executor::send(glucose::IDevice_Event *event) {
+	return mConsument->push_back(event);
+}
+
+
+HRESULT IfaceCalling CFilter_Executor::push_back(glucose::IDevice_Event *event) {
+	if (event == nullptr)
+		return E_INVALIDARG;
+
+	if (mShutting_Down_push_back)
+		return S_FALSE;
+
+	glucose::TDevice_Event *raw_event;
+	HRESULT rc = event->Raw(&raw_event);
+	if (rc != S_OK) return rc;
+
+	if (raw_event->event_code == glucose::NDevice_Event_Code::Shut_Down)
+		mShutting_Down_push_back = true;
+
+	try {
+		mQueue.push(event);
+	}
+	catch (tbb::user_abort &) {
+		mShutting_Down_push_back = true;
+		return S_FALSE;
+	}
+	return S_OK;
+}
+
+void CFilter_Executor::abort() {
+	mShutting_Down_push_back = mShutting_Down_Receive = true;
+	mQueue.abort();
+}
+
+
+
+CAsync_Filter_Executor::CAsync_Filter_Executor(const GUID filter_id, glucose::IFilter_Executor *consument, glucose::TOn_Filter_Created on_filter_created, const void* on_filter_created_data) : CFilter_Executor(consument)  {
+	mFilter = glucose::create_filter(filter_id, static_cast<glucose::IFilter_Pipe_Reader*>(this), static_cast<glucose::IFilter_Pipe_Writer*>(this));
+	//at this point, we will call a callback function to configure the filter we've just created 
+	on_filter_created(on_filter_created_data, mFilter.get());
+	//once configured, let's execute
+	mThread = std::make_unique<std::thread>([this]() {	mFilter->Execute();	});
+}
+
+
+void CAsync_Filter_Executor::join() {	
+	if (mThread)	
+		if (mThread->joinable()) 
+			mThread->join();
+}
+
+
+CSync_Filter_Executor::CSync_Filter_Executor(const GUID filter_id, glucose::IFilter_Executor *consument, glucose::TOn_Filter_Created on_filter_created, const void* on_filter_created_data) : CFilter_Executor(consument) {
+	mFilter = glucose::create_filter(filter_id, static_cast<glucose::IFilter_Pipe_Reader*>(this), static_cast<glucose::IFilter_Pipe_Writer*>(this));
+	//at this point, we will call a callback function to configure the filter we've just created 
+	on_filter_created(on_filter_created_data, mFilter.get());
+	//once configured, we can call its execute method later on
+}
+
+
+
+HRESULT IfaceCalling CSync_Filter_Executor::push_back(glucose::IDevice_Event *event) {
+	if (!mFilter) return E_FAIL;
+	HRESULT rc = CFilter_Executor::push_back(event);
+	if (rc == S_OK)
+		rc = mFilter->Execute();
+	return rc;	
+}
---
+++ factory/src/executor.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "../../../common/rtl/FilterLib.h"
+#include "../../../common/rtl/referencedImpl.h"
+
+#include <tbb/concurrent_queue.h>
+
+#include <thread>
+
+class CExecutor : public virtual glucose::IFilter_Executor {
+public:
+	virtual ~CExecutor() {}
+	
+	virtual void abort() = 0; //terminate and join
+	virtual void join() {};
+};
+
+class CFilter_Executor : public virtual CExecutor, public virtual glucose::IFilter_Pipe_Reader, public virtual glucose::IFilter_Pipe_Writer {
+protected:
+	refcnt::SReferenced<glucose::IFilter_Executor> mConsument;
+	tbb::concurrent_bounded_queue<glucose::IDevice_Event*> mQueue;
+	const std::ptrdiff_t mDefault_Capacity = 64;	
+	bool mShutting_Down_push_back = false;
+	bool mShutting_Down_Receive = false;
+	//we need two flags to let the last shutdown event to fall through
+	glucose::SFilter mFilter;
+public:
+	CFilter_Executor(glucose::IFilter_Executor *consument);
+	virtual ~CFilter_Executor() {}
+
+	virtual HRESULT IfaceCalling receive(glucose::IDevice_Event **event) override final;
+	virtual HRESULT IfaceCalling send(glucose::IDevice_Event *event) override final;
+	virtual HRESULT IfaceCalling push_back(glucose::IDevice_Event *event) override;
+	virtual void abort() override final;
+};
+
+class CAsync_Filter_Executor : public virtual CFilter_Executor {
+protected:
+	 std::unique_ptr<std::thread> mThread;
+public:
+	CAsync_Filter_Executor(const GUID filter_id, glucose::IFilter_Executor *consument, glucose::TOn_Filter_Created on_filter_created, const void* on_filter_created_data);
+	virtual ~CAsync_Filter_Executor() {};	
+	
+	virtual void join() override final;
+};
+
+class CSync_Filter_Executor : public virtual CFilter_Executor {
+protected:
+
+public:
+	CSync_Filter_Executor(const GUID filter_id, glucose::IFilter_Executor *consument, glucose::TOn_Filter_Created on_filter_created, const void* on_filter_created_data);
+	virtual ~CSync_Filter_Executor() {};
+
+	virtual HRESULT IfaceCalling push_back(glucose::IDevice_Event *event) override final;
+};
+
+class CTerminal_Executor : public virtual CExecutor {
+	//executer designed to consume events only
+public:
+	CTerminal_Executor() {};
+	virtual ~CTerminal_Executor() {};
+	virtual void abort() override  final {};
+	virtual HRESULT IfaceCalling push_back(glucose::IDevice_Event *event) override final { event->Release(); };
+};
