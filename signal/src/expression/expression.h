0a67efa1b7cc6d3c86e64766929a4f5e4fd92938
Tomáš Koutný <txkoutny@kiv.zcu.cz>
2020-02-07 10:39:47 +0100

    handling incorrect inputs in the inifile

--- signal/src/expression/expression.h
+++ signal/src/expression/expression.h
@@ -43,6 +43,7 @@
 #include <type_traits>
 
 #include "../../../../common/rtl/DeviceLib.h"
+#include "../../../../common/utils/string_utils.h"
 
 namespace expression {
     
@@ -74,16 +75,9 @@ namespace expression {
         };
     };
 
-    class CDouble : public virtual CConstant<double> {
-    protected:
-        static double Str_2_dbl(const char* str) {
-            char* end_char;
-            double tmp = std::strtod(str, &end_char);
-            if (*end_char != 0) tmp = std::numeric_limits<double>::quiet_NaN();
-            return tmp;
-        }
+    class CDouble : public virtual CConstant<double> {    
     public:
-        CDouble(const char* str) : CConstant<double>(Str_2_dbl(str)) { };
+        CDouble(const char* str) : CConstant<double>(str_2_dbl(str)) { };
     };
 
     class CNot : public virtual CAST_Node {
