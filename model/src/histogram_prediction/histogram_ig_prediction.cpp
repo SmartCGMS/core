94ff90c7e2cff0ce7d966f30a6d456349080b298
txkoutny <txkoutny@kiv.zcu.cz>
2020-04-09 15:33:33 +0200

    bugfixing

--- model/src/histogram_prediction/histogram_ig_prediction.cpp
+++ model/src/histogram_prediction/histogram_ig_prediction.cpp
@@ -647,7 +647,7 @@ HRESULT IfaceCalling CHistogram_IG_Prediction_Signal::Get_Continuous_Levels(scgm
 	for (size_t i = 0; i < count; i++) {
 		size_t band_idx;
 		hist_ig_pred::NPattern_Dir x2, x;
-		if (Classify(times[i], band_idx, x2, x)) {
+		if (Classify(times[i] - mDt, band_idx, x2, x)) {
 			const double raw_band = parameters.bands[static_cast<const size_t>(band_idx)][static_cast<const size_t>(x2)][static_cast<const size_t>(x)];
 			levels[i] = hist_ig_pred::Histogram_Index_To_Level(
 				static_cast<size_t>(round(raw_band)));			
