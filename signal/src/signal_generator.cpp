35ddc0ddf17456c18522a9c21479a4bc5ce3fad3
txkoutny <txkoutny@kiv.zcu.cz>
2020-05-28 16:56:33 +0200

    improved sig gen diag

--- signal/src/signal_generator.cpp
+++ signal/src/signal_generator.cpp
@@ -223,8 +223,10 @@ HRESULT CSignal_Generator::Do_Configure(scgms::SFilter_Configuration configurati
 	if (Is_Invalid_GUID(model_id) || Is_Any_NaN(mFixed_Stepping, mMax_Time)) return E_INVALIDARG;
 	
 	uint64_t segment_id = configuration.Read_Int(rsTime_Segment_ID, scgms::Invalid_Segment_Id);
-	if (!mSync_To_Signal && ((segment_id == scgms::Invalid_Segment_Id) || (segment_id == scgms::All_Segments_Id)))
+	if (!mSync_To_Signal && ((segment_id == scgms::Invalid_Segment_Id) || (segment_id == scgms::All_Segments_Id))) {
+		error_description.push(dsAsync_Sig_Gen_Req_Seg_Id);
 		return E_INVALIDARG;
+	}
 
 
 	if (!mSync_To_Signal && (mFixed_Stepping <= 0.0)) {
