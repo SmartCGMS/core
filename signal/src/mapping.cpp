9f6482b95f578bcaf2ff97c5aa793c0e4d58fd4b
txkoutny <txkoutny@kiv.zcu.cz>
2020-06-03 09:02:09 +0200

    log replay reports when cannot open a file mapping does not trash info signals, when mapped to null signal

--- signal/src/mapping.cpp
+++ signal/src/mapping.cpp
@@ -57,7 +57,7 @@ HRESULT IfaceCalling CMapping_Filter::Do_Configure(scgms::SFilter_Configuration 
 
 HRESULT IfaceCalling CMapping_Filter::Do_Execute(scgms::UDevice_Event event) {
     if (event.signal_id() == mSource_Id) {
-        if (mDestination_Null && !event.is_control_event()) {
+        if (mDestination_Null && !event.is_control_event() && !event.is_info_event()) {
             event.reset(nullptr);
             return S_OK;
         }
