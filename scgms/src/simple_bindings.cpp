90da3b601267d85ba7667f0f5da198e6fd640176
Martin Ubl <ublm@kiv.zcu.cz>
2020-06-29 08:43:59 +0200

    Added filter create callback to simple bindings

--- scgms/src/simple_bindings.cpp
+++ scgms/src/simple_bindings.cpp
@@ -76,13 +76,13 @@ public:
 	}
 
 
-	bool Execute_Configuration(const char* config) {	
+	bool Execute_Configuration(const char* config, scgms::TOn_Filter_Created filterCreatedCallback = nullptr, const void* filterCreatedCallbackData = nullptr) {
 
 		mErrors = refcnt::Swstr_list{};
 		scgms::SPersistent_Filter_Chain_Configuration configuration{};
 		if (configuration->Load_From_Memory(config, strlen(config), mErrors.get()) == S_OK) {
 			scgms::IFilter_Executor *executor;
-			if (execute_filter_configuration(configuration.get(), nullptr, nullptr, mCallback ? this : nullptr, &executor, mErrors.get()) == S_OK)
+			if (execute_filter_configuration(configuration.get(), filterCreatedCallback, filterCreatedCallbackData, mCallback ? this : nullptr, &executor, mErrors.get()) == S_OK)
 				mExecutor.reset(executor, [](scgms::IFilter_Executor* obj_to_release) { if (obj_to_release != nullptr) obj_to_release->Release(); });
 		}
 
@@ -162,11 +162,11 @@ public:
 
 #pragma warning( pop )
 
-scgms_execution_t SimpleCalling Execute_SCGMS_Configuration(const char *config, TSCGMS_Execution_Callback callback) {
+scgms_execution_t SimpleCalling Execute_SCGMS_Configuration(const char *config, TSCGMS_Execution_Callback callback, scgms::TOn_Filter_Created filterCreatedCallback) {
 	std::unique_ptr<CSimple_SCGMS_Execution> result = std::make_unique<CSimple_SCGMS_Execution>(callback);
 
 	if (result)
-		if (!result->Execute_Configuration(config))
+		if (!result->Execute_Configuration(config, filterCreatedCallback))
 			result.reset();
 	
 	CSimple_SCGMS_Execution* raw_result = result.get();
