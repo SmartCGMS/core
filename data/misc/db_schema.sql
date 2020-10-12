7483ca283e12809ea54234eee95b7155e7ae7b49
txkoutny <txkoutny@kiv.zcu.cz>
2020-10-12 09:26:10 +0200

    corrected db schema format

--- data/misc/db_schema.sql
+++ data/misc/db_schema.sql
@@ -1,2 +1,164 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" "http://www.w3.org/TR/REC-html40/strict.dtd">
-<html><meta http-equiv="Content-Type" content="text/html; charset=System"/><title>Exported database: fn2018</title><style type="text/css">table{border-style:solid;border-width:1px;border-color:black;border-collapse:collapse;}table tr{background-color:white;}table tr.header{background-color:#DDDDDD;}table tr.title{background-color:#EEEEEE;}table tr td{padding:0px 3px 0px 3px;border-style:solid;border-width:1px;border-color:#666666;}table tr td.null{color:#999999;text-align:center;padding:0px 3px 0px 3px;border-style:solid;border-width:1px;border-color:#666666;}table tr td.separator{padding:0px 3px 0px 3px;border-style:solid;border-width:1px;border-color:#666666;background-color:#DDDDDD;}table tr td.rownum{padding:0px 3px 0px 3px;border-style:solid;border-width:1px;border-color:#666666;background-color:#DDDDDD;text-align:right;}</style><body><table><tr class="title"><td colspan="10" align="center">Table: diagnosis</td></tr><tr class="header"><td align="right"><b><i>#</i></b></td><td><b>id</b><br/>integer</td><td><b>E08</b><br/>boolean</td><td><b>E09</b><br/>boolean</td><td><b>E10</b><br/>boolean</td><td><b>E11</b><br/>boolean</td><td><b>O24</b><br/>boolean</td><td><b>other</b><br/>boolean</td><td><b>complete</b><br/>text</td><td><b>segmentid</b><br/>bigint</td></tr></table><br/><br/><table><tr class="title"><td colspan="10" align="center">Table: difuse2params</td></tr><tr class="header"><td align="right"><b><i>#</i></b></td><td><b>id</b><br/>integer</td><td><b>p</b><br/>double precision</td><td><b>cg</b><br/>double precision</td><td><b>c</b><br/>double precision</td><td><b>dt</b><br/>double precision</td><td><b>h</b><br/>double precision</td><td><b>k</b><br/>double precision</td><td><b>s</b><br/>double precision</td><td><b>segmentid</b><br/>bigint</td></tr></table><br/><br/><table><tr class="title"><td colspan="11" align="center">Table: difuse3params</td></tr><tr class="header"><td align="right"><b><i>#</i></b></td><td><b>id</b><br/>integer</td><td><b>p</b><br/>double precision</td><td><b>cg</b><br/>double precision</td><td><b>c</b><br/>double precision</td><td><b>tau</b><br/>double precision</td><td><b>dt</b><br/>double precision</td><td><b>h</b><br/>double precision</td><td><b>kpos</b><br/>double precision</td><td><b>kneg</b><br/>double precision</td><td><b>segmentid</b><br/>bigint</td></tr></table><br/><br/><table><tr class="title"><td colspan="7" align="center">Table: difuseparams</td></tr><tr class="header"><td align="right"><b><i>#</i></b></td><td><b>id</b><br/>integer</td><td><b>p</b><br/>double precision</td><td><b>cg</b><br/>double precision</td><td><b>c</b><br/>double precision</td><td><b>dt</b><br/>double precision</td><td><b>segmentid</b><br/>bigint</td></tr></table><br/><br/><table><tr class="title"><td colspan="10" align="center">Table: measuredvalue</td></tr><tr class="header"><td align="right"><b><i>#</i></b></td><td><b>id</b><br/>integer</td><td><b>measuredat</b><br/>text</td><td><b>blood</b><br/>double precision</td><td><b>ist</b><br/>double precision</td><td><b>isig</b><br/>double precision</td><td><b>insulin</b><br/>double precision</td><td><b>carbohydrates</b><br/>double precision</td><td><b>calibration</b><br/>double precision</td><td><b>segmentid</b><br/>bigint</td></tr></table><br/><br/><table><tr class="title"><td colspan="3" align="center">Table: parallel_segment</td></tr><tr class="header"><td align="right"><b><i>#</i></b></td><td><b>id</b><br/>integer</td><td><b>dummy</b><br/>integer</td></tr></table><br/><br/><table><tr class="title"><td colspan="5" align="center">Table: segment_source</td></tr><tr class="header"><td align="right"><b><i>#</i></b></td><td><b>id</b><br/>integer</td><td><b>filepath</b><br/>text</td><td><b>device</b><br/>text</td><td><b>segmentid</b><br/>bigint</td></tr></table><br/><br/><table><tr class="title"><td colspan="8" align="center">Table: steilrebrinparams</td></tr><tr class="header"><td align="right"><b><i>#</i></b></td><td><b>id</b><br/>integer</td><td><b>t</b><br/>double precision</td><td><b>g</b><br/>double precision</td><td><b>alpha</b><br/>double precision</td><td><b>beta</b><br/>double precision</td><td><b>gamma</b><br/>double precision</td><td><b>segmentid</b><br/>bigint</td></tr></table><br/><br/><table><tr class="title"><td colspan="7" align="center">Table: subject</td></tr><tr class="header"><td align="right"><b><i>#</i></b></td><td><b>id</b><br/>integer</td><td><b>name</b><br/>text</td><td><b>comments</b><br/>text</td><td><b>sex</b><br/>integer</td><td><b>weight</b><br/>integer</td><td><b>user_id</b><br/>bigint</td></tr></table><br/><br/><table><tr class="title"><td colspan="7" align="center">Table: timesegment</td></tr><tr class="header"><td align="right"><b><i>#</i></b></td><td><b>id</b><br/>integer</td><td><b>name</b><br/>text</td><td><b>comment</b><br/>text</td><td><b>subjectid</b><br/>bigint</td><td><b>parallel_id</b><br/>bigint</td><td><b>deleted</b><br/>boolean</td></tr></table><br/><br/><i>Document generated by SQLiteStudio v3.1.1 on po øíj 12 09:13:26 2020</i></body></html>
+--
+-- File generated with SQLiteStudio v3.1.1 on po øíj 12 09:24:59 2020
+--
+-- Text encoding used: System
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Table: diagnosis
+CREATE TABLE diagnosis (
+    id        INTEGER PRIMARY KEY AUTOINCREMENT,
+    E08       BOOLEAN NOT NULL,
+    E09       BOOLEAN NOT NULL,
+    E10       BOOLEAN NOT NULL,
+    E11       BOOLEAN NOT NULL,
+    O24       BOOLEAN NOT NULL,
+    other     BOOLEAN NOT NULL,
+    complete  TEXT    NOT NULL,
+    segmentid BIGINT,
+    CONSTRAINT fk_diagnosis_segmentid FOREIGN KEY (
+        segmentid
+    )
+    REFERENCES timesegment (id) DEFERRABLE INITIALLY DEFERRED
+);
+
+
+-- Table: difuse2params
+CREATE TABLE difuse2params (
+    id        INTEGER            PRIMARY KEY AUTOINCREMENT,
+    p         [DOUBLE PRECISION],
+    cg        [DOUBLE PRECISION],
+    c         [DOUBLE PRECISION],
+    dt        [DOUBLE PRECISION],
+    h         [DOUBLE PRECISION],
+    k         [DOUBLE PRECISION],
+    s         [DOUBLE PRECISION],
+    segmentid BIGINT,
+    CONSTRAINT fk_difuse2params_segmentid FOREIGN KEY (
+        segmentid
+    )
+    REFERENCES timesegment (id) DEFERRABLE INITIALLY DEFERRED
+);
+
+
+-- Table: difuse3params
+CREATE TABLE difuse3params (
+    id        INTEGER            PRIMARY KEY AUTOINCREMENT,
+    p         [DOUBLE PRECISION],
+    cg        [DOUBLE PRECISION],
+    c         [DOUBLE PRECISION],
+    tau       [DOUBLE PRECISION],
+    dt        [DOUBLE PRECISION],
+    h         [DOUBLE PRECISION],
+    kpos      [DOUBLE PRECISION],
+    kneg      [DOUBLE PRECISION],
+    segmentid BIGINT,
+    CONSTRAINT fk_difuse3params_segmentid FOREIGN KEY (
+        segmentid
+    )
+    REFERENCES timesegment (id) DEFERRABLE INITIALLY DEFERRED
+);
+
+
+-- Table: difuseparams
+CREATE TABLE difuseparams (
+    id        INTEGER            PRIMARY KEY AUTOINCREMENT,
+    p         [DOUBLE PRECISION],
+    cg        [DOUBLE PRECISION],
+    c         [DOUBLE PRECISION],
+    dt        [DOUBLE PRECISION],
+    segmentid BIGINT,
+    CONSTRAINT fk_difuseparams_segmentid FOREIGN KEY (
+        segmentid
+    )
+    REFERENCES timesegment (id) DEFERRABLE INITIALLY DEFERRED
+);
+
+
+-- Table: measuredvalue
+CREATE TABLE measuredvalue (
+    id            INTEGER            PRIMARY KEY AUTOINCREMENT,
+    measuredat    TEXT,
+    blood         [DOUBLE PRECISION],
+    ist           [DOUBLE PRECISION],
+    isig          [DOUBLE PRECISION],
+    insulin       [DOUBLE PRECISION],
+    carbohydrates [DOUBLE PRECISION],
+    calibration   [DOUBLE PRECISION],
+    segmentid     BIGINT,
+    CONSTRAINT fk_measuredvalue_segmentid FOREIGN KEY (
+        segmentid
+    )
+    REFERENCES timesegment (id) DEFERRABLE INITIALLY DEFERRED
+);
+
+
+-- Table: parallel_segment
+CREATE TABLE parallel_segment (
+    id    INTEGER PRIMARY KEY AUTOINCREMENT,
+    dummy INTEGER NOT NULL
+);
+
+
+-- Table: segment_source
+CREATE TABLE segment_source (
+    id        INTEGER PRIMARY KEY AUTOINCREMENT,
+    filepath  TEXT    NOT NULL,
+    device    TEXT    NOT NULL,
+    segmentid BIGINT,
+    CONSTRAINT fk_segment_source_segmentid FOREIGN KEY (
+        segmentid
+    )
+    REFERENCES timesegment (id) DEFERRABLE INITIALLY DEFERRED
+);
+
+
+-- Table: steilrebrinparams
+CREATE TABLE steilrebrinparams (
+    id        INTEGER            PRIMARY KEY AUTOINCREMENT,
+    t         [DOUBLE PRECISION],
+    g         [DOUBLE PRECISION],
+    alpha     [DOUBLE PRECISION],
+    beta      [DOUBLE PRECISION],
+    gamma     [DOUBLE PRECISION],
+    segmentid BIGINT,
+    CONSTRAINT fk_steilrebrinparams_segmentid FOREIGN KEY (
+        segmentid
+    )
+    REFERENCES timesegment (id) DEFERRABLE INITIALLY DEFERRED
+);
+
+
+-- Table: subject
+CREATE TABLE subject (
+    id       INTEGER PRIMARY KEY AUTOINCREMENT,
+    name     TEXT,
+    comments TEXT,
+    sex      INTEGER,
+    weight   INTEGER,
+    user_id  BIGINT
+);
+
+
+-- Table: timesegment
+CREATE TABLE timesegment (
+    id          INTEGER PRIMARY KEY AUTOINCREMENT,
+    name        TEXT,
+    comment     TEXT,
+    subjectid   BIGINT,
+    parallel_id BIGINT,
+    deleted     BOOLEAN,
+    CONSTRAINT fk_timesegment_subjectid FOREIGN KEY (
+        subjectid
+    )
+    REFERENCES subject (id) DEFERRABLE INITIALLY DEFERRED,
+    CONSTRAINT fk_timesegment_parallel FOREIGN KEY (
+        parallel_id
+    )
+    REFERENCES parallel_segment (id) DEFERRABLE INITIALLY DEFERRED
+);
+
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
