70ce7768c0019368b1845b74c5bd83e78ea30712
Tomáš Koutný <txkoutny@kiv.zcu.cz>
2022-03-04 10:55:24 +0100

    bugfixed basal 2 bolus descriptor

--- controllers/src/descriptor.cpp
+++ controllers/src/descriptor.cpp
@@ -58,11 +58,11 @@
 
 
 namespace basal_2_bolus {
-	const size_t filter_param_count = model_param_count;
-	const scgms::NParameter_Type filter_param_types[filter_param_count] = { scgms::NParameter_Type::ptDouble, scgms::NParameter_Type::ptRatTime };
-	const wchar_t* filter_param_ui_names[filter_param_count] = { dsMinimum_Volume, dsPPeriod};
-	const wchar_t* filter_param_config_names[filter_param_count] = { rsMinimum, rsPeriod };
-	const wchar_t* filter_param_tooltips[filter_param_count] = { nullptr, nullptr};
+	const size_t filter_param_count = 1;
+	const scgms::NParameter_Type filter_param_types[filter_param_count] = { scgms::NParameter_Type::ptDouble_Array};
+	const wchar_t* filter_param_ui_names[filter_param_count] = { dsParameters};
+	const wchar_t* filter_param_config_names[filter_param_count] = { rsParameters };
+	const wchar_t* filter_param_tooltips[filter_param_count] = { nullptr};
 
 	const scgms::TFilter_Descriptor filter_desc = {
 				filter_id,
