b1e6c63ad68e1349cae3e18a4ed9ec9c482758eb
Tomáš Koutný <txkoutny@kiv.zcu.cz>
2023-01-31 22:47:44 +0100

    improved pattern prediction self-learning

--- model/src/pattern_prediction/pattern_prediction_data.cpp
+++ model/src/pattern_prediction/pattern_prediction_data.cpp
@@ -72,7 +72,67 @@ void CPattern_Prediction_Data::push(const double device_time, const double level
 }
 
 
-double CPattern_Prediction_Data::predict() {       
+double CPattern_Prediction_Data::predict() {
+    if (mInvalidated) {
+        const size_t full_set_n = mFull ? mState.size() : mHead;
+
+        bool is_mard_small_enough = full_set_n >= 2;
+        if (is_mard_small_enough) {
+
+            double full_set_avg = 0.0;
+            for (size_t i = 0; i < full_set_n; i++) {
+                full_set_avg += mState[i];
+            }
+            full_set_avg /= static_cast<double>(full_set_n);
+            //full_set_avg provides the best absolute-error fit
+            //let's use it, if its MARD does not exceed a specific threshold
+
+            constexpr size_t candidate_count = 8;   //8 to support auto vectorization
+            constexpr std::array<double, candidate_count> candidate_offsets = { {0.985, 0.990, 0.995, 1.000, 1.005, 1.010, 1.015, 1.020} };
+            std::array<double, candidate_count> candidate_prediction, mards;
+            for (size_t i = 0; i < candidate_offsets.size(); i++) {
+                candidate_prediction[i] = candidate_offsets[i] * full_set_avg;
+                mards[i] = 0.0;
+            }
+
+            //compute candidate mard sums - actually, we mimic a more sophisticated solver by computing hard-coded guess solutions
+            double MARD_n = 0.0;
+            for (size_t i = 0; i < full_set_n; i++) {
+                const double current_level = mState[i];
+                if (current_level > 0.0) {
+
+                    const double inv_current_level = 1.0 / current_level;
+                    for (size_t j = 0; j < candidate_prediction.size(); j++)
+                        mards[j] += fabs(current_level - candidate_prediction[j]) * inv_current_level;
+
+                    MARD_n += 1.0;
+                }
+            }
+
+            //find the least sum
+            is_mard_small_enough = MARD_n >= 1.9;   //if there are enough data --1.9 accounts rounding errors
+            if (is_mard_small_enough) {
+                const auto least_mard_iter = std::min_element(mards.begin(), mards.end());
+                //check that the least mard is really small enough
+                is_mard_small_enough = *least_mard_iter < MARD_n* pattern_prediction::Relative_Error_Correct_Prediction_Threshold;  //multiplication is cheaper
+
+                if (is_mard_small_enough) {
+                    mRecent_Prediction = candidate_prediction[std::distance(mards.begin(), least_mard_iter)];
+                }
+            }
+        }
+
+        if (!is_mard_small_enough) 
+            mRecent_Prediction = mState[full_set_n - 1];    //relative error is too big, let's reuse the recent level
+           
+
+        mInvalidated = false;
+    }
+
+    return mRecent_Prediction;
+}
+
+double CPattern_Prediction_Data::predict_doi_10_dot_1016_slash_j_compbiomed_dot_2022_dot_105388() {
     if (mInvalidated) {
 
         const size_t full_set_n = mFull ? mState.size() : mHead;
@@ -240,9 +300,9 @@ std::stringstream CPattern_Prediction_Data::Level_Series() const {
     for (const auto e : mLearning_Data) {
         if (fired_once)
             result << " ";
-         fired_once = true;
+        fired_once = true;
 
-         result << dbl_2_str(e.level);
+        result << dbl_2_str(e.level);
     }
 
     return result;
