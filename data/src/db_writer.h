4316d9f7fafc456cec4def10c0f234cdd05b68f4
Martin Ubl <ublm@kiv.zcu.cz>
2023-06-22 11:38:49 +0200

    Fixed database writer after unsuccessful merge

--- data/src/db_writer.h
+++ data/src/db_writer.h
@@ -10,8 +10,8 @@
  * Faculty of Applied Sciences, University of West Bohemia
  * Univerzitni 8, 301 00 Pilsen
  * Czech Republic
- * 
- * 
+ *
+ *
  * Purpose of this software:
  * This software is intended to demonstrate work of the diabetes.zcu.cz research
  * group to other scientists, to complement our published papers. It is strictly
@@ -28,11 +28,11 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *
  * a) Without a specific agreement, you are not authorized to make or keep any copies of this file.
- * b) For any use, especially commercial use, you must contact us and obtain specific terms and conditions 
+ * b) For any use, especially commercial use, you must contact us and obtain specific terms and conditions
  *    for the use of the software.
  * c) When publishing any derivative work or results obtained using this software, you agree to cite the following paper:
- *    Tomas Koutny and Martin Ubl, "SmartCGMS as a Testbed for a Blood-Glucose Level Prediction and/or 
- *    Control Challenge with (an FDA-Accepted) Diabetic Patient Simulation", Procedia Computer Science,  
+ *    Tomas Koutny and Martin Ubl, "SmartCGMS as a Testbed for a Blood-Glucose Level Prediction and/or
+ *    Control Challenge with (an FDA-Accepted) Diabetic Patient Simulation", Procedia Computer Science,
  *    Volume 177, pp. 354-362, 2020
  */
 
@@ -53,75 +53,79 @@
 
 struct CPrepared_Value
 {
-    double measuredAt;
-    GUID signalId;
-    double value;
-    int64_t segmentId;
+	double measuredAt;
+	GUID signalId;
+	double value;
+	uint64_t segmentId;
 };
 
 /*
  * Filter class for writing data and parameters to database
  */
 class CDb_Writer : public scgms::CBase_Filter, public db::IDb_Sink {
-    protected:
-        // database host configured
-        std::wstring mDbHost;
-        // configured DB port
-        uint16_t mDbPort = 0;
-        // database provider string in Qt format
-        std::wstring mDbProvider;
-        // database name / filename
-        std::wstring mDbDatabaseName;
-        // username to be used
-        std::wstring mDbUsername;
-        // password to be used
-        std::wstring mDbPassword;
-        // subject id to be used
-        int64_t mSubject_Id = db::New_Subject_Identifier;
+	protected:
+		// database host configured
+		std::wstring mDbHost;
+		// configured DB port
+		uint16_t mDbPort = 0;
+		// database provider string in Qt format
+		std::wstring mDbProvider;
+		// database name / filename
+		std::wstring mDbDatabaseName;
+		// username to be used
+		std::wstring mDbUsername;
+		// password to be used
+		std::wstring mDbPassword;
+		// subject id to be used
+		int64_t mSubject_Id = db::New_Subject_Identifier;
 
-        // generate new primary keys for all incoming data intended to be stored?
-        bool mGenerate_Primary_Keys = false;
-        // store incoming data (levels, masked levels, ...)?
-        bool mStore_Data = false;
-        // store parameters?
-        bool mStore_Parameters = false;
+		// generate new primary keys for all incoming data intended to be stored?
+		bool mGenerate_Primary_Keys = false;
+		// store incoming data (levels, masked levels, ...)?
+		bool mStore_Data = false;
+		// store parameters?
+		bool mStore_Parameters = false;
 
-        // mapping of simulator segment IDs to database IDs
-        std::map<int64_t, int64_t> mSegment_Db_Id_Map;
-        // set of all signals to be ignored when storing to DB
-        std::set<GUID> mIgnored_Signals;
-        // list of prepared values
-        std::list<CPrepared_Value> mPrepared_Values;
+		// mapping of simulator segment IDs to database IDs
+		std::map<int64_t, int64_t> mSegment_Db_Id_Map;
+		// set of all signals to be ignored when storing to DB
+		std::set<GUID> mIgnored_Signals;
+		// list of prepared values
+		std::list<CPrepared_Value> mPrepared_Values;
 
-        int64_t mLast_Generated_Idx = 0;
+		int64_t mLast_Generated_Idx = 0;
 
-    protected:
-        db::SDb_Connector mDb_Connector;
-        db::SDb_Connection mDb_Connection;
+		bool mConnected = false;
 
-        int64_t Get_Db_Segment_Id(int64_t segment_id);
+		HRESULT Connect_To_Db_If_Needed();
 
-        // generates a new timesegment, assigns a new id (primary key) and stores into database
-        int64_t Create_Segment(std::wstring name, std::wstring comment);
-        // generated a new subject
-        int64_t Create_Subject(std::wstring name);
+	protected:
+		db::SDb_Connector mDb_Connector;
+		db::SDb_Connection mDb_Connection;
 
-        // stores incoming level to database
-        bool Store_Level(const scgms::UDevice_Event& evt);
-        // stores incoming parameters to database
-        bool Store_Parameters(const scgms::UDevice_Event& evt);
+		int64_t Get_Db_Segment_Id(int64_t segment_id);
 
-        // flushes cached levels to database
-        void Flush_Levels();
+		// generates a new timesegment, assigns a new id (primary key) and stores into database
+		int64_t Create_Segment(std::wstring name);
+		// generated a new subject
+		int64_t Create_Subject(std::wstring name);
 
-        virtual HRESULT Do_Execute(scgms::UDevice_Event event) override final;
-        HRESULT Do_Configure(scgms::SFilter_Configuration configuration, refcnt::Swstr_list& error_description) override final;
-    public:
-        CDb_Writer(scgms::IFilter *output);
-        virtual ~CDb_Writer();
+		// stores incoming level to database
+		bool Store_Level(const scgms::UDevice_Event& evt);
+		// stores incoming parameters to database
+		bool Store_Parameters(const scgms::UDevice_Event& evt);
 
-        virtual HRESULT IfaceCalling QueryInterface(const GUID*  riid, void ** ppvObj) override;		
-        virtual HRESULT IfaceCalling Set_Connector(db::IDb_Connector *connector) override;
+		// flushes cached levels to database
+		void Flush_Levels();
+
+		virtual HRESULT Do_Execute(scgms::UDevice_Event event) override final;
+		HRESULT Do_Configure(scgms::SFilter_Configuration configuration, refcnt::Swstr_list& error_description) override final;
+	public:
+		CDb_Writer(scgms::IFilter* output);
+		virtual ~CDb_Writer();
+
+		virtual HRESULT IfaceCalling QueryInterface(const GUID* riid, void** ppvObj) override;
+		virtual HRESULT IfaceCalling Set_Connector(db::IDb_Connector* connector) override;
 };
 
 #pragma warning( pop )
