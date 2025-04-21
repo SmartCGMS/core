0c22ce5ef0b36b32126ceb3fbcd9d99ed72c729b
MartinUbl <ublm@kiv.zcu.cz>
2025-04-21 10:41:08 +0200

    metric/grids: fixed formatting and invalid character in diabetes grids

--- metric/src/diabetes_grid/clarke_error_grid.cpp
+++ metric/src/diabetes_grid/clarke_error_grid.cpp
@@ -38,89 +38,96 @@
 
 #include <scgms/iface/DeviceIface.h>
 
-
 // The coordinates were adopted from Matlab CLARKE code
 // by Edgar Guevara Codina (codina@REMOVETHIScactus.iico.uaslp.mx)
 // File Version 1.2
 // March 29 2013
 // see http://www.mathworks.com/matlabcentral/fileexchange/20545-clarke-error-grid-analysis/content/clarke.m
 
+// Clarke error grid coordinates
+#pragma region ClarkeCoords
 
-//plot([0 400], [0 400], 'k:') % Theoretical 45 regression line
-//plot([0 175 / 3], [70 70], 'k-')
-//% plot([175 / 3 320], [70 400], 'k-')
-//plot([175 / 3 400 / 1.2], [70 400], 'k-') % replace 320 with 400 / 1.2 because 100 * (400 - 400 / 1.2) / (400 / 1.2) = 20 % error
-//plot([70 70], [84 400], 'k-')
-//plot([0 70], [180 180], 'k-')
-//plot([70 290], [180 400], 'k-') % Corrected upper B - C boundary
-//% plot([70 70], [0 175 / 3], 'k-')
-//plot([70 70], [0 56], 'k-') % replace 175.3 with 56 because 100 * abs(56 - 70) / 70) = 20 % error
-//% plot([70 400], [175 / 3 320], 'k-')
-//plot([70 400], [56 320], 'k-')
-//plot([180 180], [0 70], 'k-')
-//plot([180 400], [70 70], 'k-')
-//plot([240 240], [70 180], 'k-')
-//plot([240 400], [180 180], 'k-')
-//plot([130 180], [0 70], 'k-') % Lower B - C boundary slope OK
-
-
-#pragma region Diabetes Coords
 const std::vector<TError_Grid_Point> CLARKE_ZONE_A = {
-	{ 70.0*scgms::mgdL_2_mmolL, 0.0 },{ 70.0 * scgms::mgdL_2_mmolL, 56.0 * scgms::mgdL_2_mmolL },{ 400.0 * scgms::mgdL_2_mmolL, 320.0 * scgms::mgdL_2_mmolL },
-
-	{ 400.0 * scgms::mgdL_2_mmolL, 400.0 * scgms::mgdL_2_mmolL },
-	{ 400.0 * scgms::mgdL_2_mmolL / (1.2 * scgms::mgdL_2_mmolL), 400.0 * scgms::mgdL_2_mmolL },{70.0 * scgms::mgdL_2_mmolL,84.0 * scgms::mgdL_2_mmolL}, { 175.0 * scgms::mgdL_2_mmolL / (3.0 * scgms::mgdL_2_mmolL), 70.0 * scgms::mgdL_2_mmolL }, { 0.0, 70.0 * scgms::mgdL_2_mmolL },
-	{ 0.0, 0.0 },
+	{ 70.0 * scgms::mgdL_2_mmolL,                                0.0 },
+	{ 70.0 * scgms::mgdL_2_mmolL,                                56.0 * scgms::mgdL_2_mmolL },
+	{ 400.0 * scgms::mgdL_2_mmolL,                               320.0 * scgms::mgdL_2_mmolL },
+	{ 400.0 * scgms::mgdL_2_mmolL,                               400.0 * scgms::mgdL_2_mmolL },
+	{ 400.0 * scgms::mgdL_2_mmolL / (1.2 * scgms::mgdL_2_mmolL), 400.0 * scgms::mgdL_2_mmolL },
+	{ 70.0 * scgms::mgdL_2_mmolL,                                84.0 * scgms::mgdL_2_mmolL},
+	{ 175.0 * scgms::mgdL_2_mmolL / (3.0 * scgms::mgdL_2_mmolL), 70.0 * scgms::mgdL_2_mmolL },
+	{ 0.0,                                                       70.0 * scgms::mgdL_2_mmolL },
+	{ 0.0,                                                       0.0 },
 };
 
 const std::vector<TError_Grid_Point> CLARKE_ZONE_B_Up = {
-	{ 70.0 * scgms::mgdL_2_mmolL, 84.0 * scgms::mgdL_2_mmolL },{ 400.0 * scgms::mgdL_2_mmolL / (1.2 * scgms::mgdL_2_mmolL), 400.0 * scgms::mgdL_2_mmolL },
-	{290.0 * scgms::mgdL_2_mmolL, 400.0 * scgms::mgdL_2_mmolL}, {70.0 * scgms::mgdL_2_mmolL, 180.0 * scgms::mgdL_2_mmolL},
+	{ 70.0 * scgms::mgdL_2_mmolL,                                84.0 * scgms::mgdL_2_mmolL },
+	{ 400.0 * scgms::mgdL_2_mmolL / (1.2 * scgms::mgdL_2_mmolL), 400.0 * scgms::mgdL_2_mmolL },
+	{ 290.0 * scgms::mgdL_2_mmolL,                               400.0 * scgms::mgdL_2_mmolL },
+	{ 70.0 * scgms::mgdL_2_mmolL,                                180.0 * scgms::mgdL_2_mmolL },
 };
 
 const std::vector<TError_Grid_Point> CLARKE_ZONE_C_Up = {
-	{ 70.0 * scgms::mgdL_2_mmolL, 180.0 * scgms::mgdL_2_mmolL },{ 290.0 * scgms::mgdL_2_mmolL, 400.0 * scgms::mgdL_2_mmolL },
-	{ 70.0 * scgms::mgdL_2_mmolL, 400.0 * scgms::mgdL_2_mmolL}
+	{ 70.0 * scgms::mgdL_2_mmolL,  180.0 * scgms::mgdL_2_mmolL },
+	{ 290.0 * scgms::mgdL_2_mmolL, 400.0 * scgms::mgdL_2_mmolL },
+	{ 70.0 * scgms::mgdL_2_mmolL,  400.0 * scgms::mgdL_2_mmolL }
 };
 
 const std::vector<TError_Grid_Point>  CLARKE_ZONE_D_Up = {
-	{ 0.0, 70.0 * scgms::mgdL_2_mmolL },{ 175 / (3.0 * scgms::mgdL_2_mmolL), 70.0 * scgms::mgdL_2_mmolL },{ 70.0 * scgms::mgdL_2_mmolL,84.0 * scgms::mgdL_2_mmolL },
-	{70.0 * scgms::mgdL_2_mmolL, 180.0 * scgms::mgdL_2_mmolL}, {0.0, 180.0 * scgms::mgdL_2_mmolL},
+	{ 0.0,                               70.0 * scgms::mgdL_2_mmolL },
+	{ 175 / (3.0 * scgms::mgdL_2_mmolL), 70.0 * scgms::mgdL_2_mmolL },
+	{ 70.0 * scgms::mgdL_2_mmolL,        84.0 * scgms::mgdL_2_mmolL },
+	{ 70.0 * scgms::mgdL_2_mmolL,        180.0 * scgms::mgdL_2_mmolL },
+	{ 0.0,                               180.0 * scgms::mgdL_2_mmolL },
 };
 
 const std::vector<TError_Grid_Point> CLARKE_ZONE_E_Up = {
 	{ 70.0 * scgms::mgdL_2_mmolL, 180.0 * scgms::mgdL_2_mmolL },
-	{ 70.0 * scgms::mgdL_2_mmolL, 400.0 * scgms::mgdL_2_mmolL },{ 0.0, 400.0 * scgms::mgdL_2_mmolL },
-	{ 0.0, 180.0 * scgms::mgdL_2_mmolL },
+	{ 70.0 * scgms::mgdL_2_mmolL, 400.0 * scgms::mgdL_2_mmolL },
+	{ 0.0,                        400.0 * scgms::mgdL_2_mmolL },
+	{ 0.0,                        180.0 * scgms::mgdL_2_mmolL },
 };
 
 const std::vector<TError_Grid_Point> CLARKE_ZONE_B_Low = {
-	{130.0 * scgms::mgdL_2_mmolL,0.0}, {180.0 * scgms::mgdL_2_mmolL,70.0 * scgms::mgdL_2_mmolL}, {240.0 * scgms::mgdL_2_mmolL,70.0 * scgms::mgdL_2_mmolL}, {240.0 * scgms::mgdL_2_mmolL,180.0 * scgms::mgdL_2_mmolL}, {400.0 * scgms::mgdL_2_mmolL, 180.0 * scgms::mgdL_2_mmolL},
-	{400.0 * scgms::mgdL_2_mmolL,320.0 * scgms::mgdL_2_mmolL}, {70.0 * scgms::mgdL_2_mmolL, 56.0 * scgms::mgdL_2_mmolL}, { 70.0 * scgms::mgdL_2_mmolL, 0.0 },
+	{ 130.0 * scgms::mgdL_2_mmolL, 0.0 },
+	{ 180.0 * scgms::mgdL_2_mmolL, 70.0 * scgms::mgdL_2_mmolL },
+	{ 240.0 * scgms::mgdL_2_mmolL, 70.0 * scgms::mgdL_2_mmolL },
+	{ 240.0 * scgms::mgdL_2_mmolL, 180.0 * scgms::mgdL_2_mmolL },
+	{ 400.0 * scgms::mgdL_2_mmolL, 180.0 * scgms::mgdL_2_mmolL },
+	{ 400.0 * scgms::mgdL_2_mmolL, 320.0 * scgms::mgdL_2_mmolL },
+	{ 70.0 * scgms::mgdL_2_mmolL,  56.0 * scgms::mgdL_2_mmolL },
+	{ 70.0 * scgms::mgdL_2_mmolL,  0.0 },
 };
 
 const std::vector<TError_Grid_Point> CLARKE_ZONE_C_Low = {
-	{180.0 * scgms::mgdL_2_mmolL, 0.0}, { 180.0 * scgms::mgdL_2_mmolL,70.0 * scgms::mgdL_2_mmolL },{ 130.0 * scgms::mgdL_2_mmolL,0.0 },
+	{ 180.0 * scgms::mgdL_2_mmolL, 0.0 },
+	{ 180.0 * scgms::mgdL_2_mmolL, 70.0 * scgms::mgdL_2_mmolL },
+	{ 130.0 * scgms::mgdL_2_mmolL, 0.0 },
 };
 
 const std::vector<TError_Grid_Point>  CLARKE_ZONE_D_Low = {
-	{ 240.0 * scgms::mgdL_2_mmolL,70.0 * scgms::mgdL_2_mmolL },{ 400.0 * scgms::mgdL_2_mmolL, 70.0 * scgms::mgdL_2_mmolL },{ 400.0 * scgms::mgdL_2_mmolL,180.0 * scgms::mgdL_2_mmolL },{240.0 * scgms::mgdL_2_mmolL,180.0 * scgms::mgdL_2_mmolL},
+	{ 240.0 * scgms::mgdL_2_mmolL, 70.0 * scgms::mgdL_2_mmolL },
+	{ 400.0 * scgms::mgdL_2_mmolL, 70.0 * scgms::mgdL_2_mmolL },
+	{ 400.0 * scgms::mgdL_2_mmolL, 180.0 * scgms::mgdL_2_mmolL },
+	{ 240.0 * scgms::mgdL_2_mmolL, 180.0 * scgms::mgdL_2_mmolL },
 };
 
 const std::vector<TError_Grid_Point>  CLARKE_ZONE_E_Low = {
-	{ 180.0 * scgms::mgdL_2_mmolL, 0.0 },{ 400.0 * scgms::mgdL_2_mmolL, 0.0 },{ 400.0 * scgms::mgdL_2_mmolL,70.0 * scgms::mgdL_2_mmolL },{ 180.0 * scgms::mgdL_2_mmolL, 70.0 * scgms::mgdL_2_mmolL },
+	{ 180.0 * scgms::mgdL_2_mmolL, 0.0 },
+	{ 400.0 * scgms::mgdL_2_mmolL, 0.0 },
+	{ 400.0 * scgms::mgdL_2_mmolL, 70.0 * scgms::mgdL_2_mmolL },
+	{ 180.0 * scgms::mgdL_2_mmolL, 70.0 * scgms::mgdL_2_mmolL },
 };
 
 #pragma endregion
 
 const TError_Grid Clarke_Error_Grid = {
-	{ NError_Grid_Zone::A, CLARKE_ZONE_A},
-	{ NError_Grid_Zone::B, CLARKE_ZONE_B_Up},
-	{ NError_Grid_Zone::B, CLARKE_ZONE_B_Low},
-	{ NError_Grid_Zone::C, CLARKE_ZONE_C_Up},
-	{ NError_Grid_Zone::C, CLARKE_ZONE_C_Low},
-	{ NError_Grid_Zone::D, CLARKE_ZONE_D_Up},
-	{ NError_Grid_Zone::D, CLARKE_ZONE_D_Low},
-	{ NError_Grid_Zone::E, CLARKE_ZONE_E_Up},
+	{ NError_Grid_Zone::A, CLARKE_ZONE_A },
+	{ NError_Grid_Zone::B, CLARKE_ZONE_B_Up },
+	{ NError_Grid_Zone::B, CLARKE_ZONE_B_Low },
+	{ NError_Grid_Zone::C, CLARKE_ZONE_C_Up },
+	{ NError_Grid_Zone::C, CLARKE_ZONE_C_Low },
+	{ NError_Grid_Zone::D, CLARKE_ZONE_D_Up },
+	{ NError_Grid_Zone::D, CLARKE_ZONE_D_Low },
+	{ NError_Grid_Zone::E, CLARKE_ZONE_E_Up },
 	{ NError_Grid_Zone::E, CLARKE_ZONE_E_Low },
 };
--- metric/src/diabetes_grid/parkes_error_grid.cpp
+++ metric/src/diabetes_grid/parkes_error_grid.cpp
@@ -39,130 +39,213 @@
 #include <scgms/iface/DeviceIface.h>
 
 
-//See also:
-//Pfützner, A., Klonoff, D.C., Pardo, S., &Parkes, J.L. (2013).
-//Technical Aspects of the Parkes Error Grid.Journal of Diabetes Science and Technology, 7(5), 1275–1281.
-//http://www.ncbi.nlm.nih.gov/pmc/articles/PMC3876371/
+// See also:
+// Pfutzner, A., Klonoff, D.C., Pardo, S., and Parkes, J.L. (2013).
+// Technical Aspects of the Parkes Error Grid.Journal of Diabetes Science and Technology, 7(5), 1275-1281.
+// http://www.ncbi.nlm.nih.gov/pmc/articles/PMC3876371/
 
+// Parkes error grid for Diabetes Type 1
+#pragma region ParkesType1Coords
 
-//Diabetes Type 1 coordinates
-#pragma region DiabetesType1
 const std::vector<TError_Grid_Point> PARKES_ZONE_A_T1 = {
-	{50.0*scgms::mgdL_2_mmolL, 0.0}, {50.0 * scgms::mgdL_2_mmolL, 30.0 * scgms::mgdL_2_mmolL}, {170.0 * scgms::mgdL_2_mmolL, 145.0 * scgms::mgdL_2_mmolL}, {385.0 * scgms::mgdL_2_mmolL, 300.0 * scgms::mgdL_2_mmolL}, {550.0 * scgms::mgdL_2_mmolL,450.0 * scgms::mgdL_2_mmolL},
-	{550.0 * scgms::mgdL_2_mmolL, 550.0 * scgms::mgdL_2_mmolL},
-	{430.0 * scgms::mgdL_2_mmolL, 550.0 * scgms::mgdL_2_mmolL}, {280.0 * scgms::mgdL_2_mmolL,380.0 * scgms::mgdL_2_mmolL}, {140.0 * scgms::mgdL_2_mmolL, 170.0 * scgms::mgdL_2_mmolL}, {30.0 * scgms::mgdL_2_mmolL, 50.0 * scgms::mgdL_2_mmolL}, {0.0 , 50.0 * scgms::mgdL_2_mmolL},
-	{0.0 , 0.0},
+	{ 50.0 * scgms::mgdL_2_mmolL,  0.0 },
+	{ 50.0 * scgms::mgdL_2_mmolL,  30.0 * scgms::mgdL_2_mmolL },
+	{ 170.0 * scgms::mgdL_2_mmolL, 145.0 * scgms::mgdL_2_mmolL },
+	{ 385.0 * scgms::mgdL_2_mmolL, 300.0 * scgms::mgdL_2_mmolL },
+	{ 550.0 * scgms::mgdL_2_mmolL, 450.0 * scgms::mgdL_2_mmolL },
+	{ 550.0 * scgms::mgdL_2_mmolL, 550.0 * scgms::mgdL_2_mmolL },
+	{ 430.0 * scgms::mgdL_2_mmolL, 550.0 * scgms::mgdL_2_mmolL },
+	{ 280.0 * scgms::mgdL_2_mmolL, 380.0 * scgms::mgdL_2_mmolL },
+	{ 140.0 * scgms::mgdL_2_mmolL, 170.0 * scgms::mgdL_2_mmolL },
+	{ 30.0 * scgms::mgdL_2_mmolL,  50.0 * scgms::mgdL_2_mmolL },
+	{ 0.0,                         50.0 * scgms::mgdL_2_mmolL },
+	{ 0.0,                         0.0 },
 };
 
 const std::vector<TError_Grid_Point> PARKES_ZONE_B_Up_T1 = {
-	{0.0 , 50.0 * scgms::mgdL_2_mmolL }, {30.0 * scgms::mgdL_2_mmolL, 50.0 * scgms::mgdL_2_mmolL},{ 140.0 * scgms::mgdL_2_mmolL, 170.0 * scgms::mgdL_2_mmolL },{ 280.0 *scgms::mgdL_2_mmolL,380.0 *scgms::mgdL_2_mmolL},{ 430.0 *scgms::mgdL_2_mmolL, 550.0 *scgms::mgdL_2_mmolL},
-	{260.0 *scgms::mgdL_2_mmolL,550.0 * scgms::mgdL_2_mmolL},{70.0 *scgms::mgdL_2_mmolL,110.0 * scgms::mgdL_2_mmolL},{50.0 *scgms::mgdL_2_mmolL,80.0 * scgms::mgdL_2_mmolL},{30.0 *scgms::mgdL_2_mmolL,60.0 * scgms::mgdL_2_mmolL},{0.0 *scgms::mgdL_2_mmolL,60.0 * scgms::mgdL_2_mmolL}
+	{ 0.0,                         50.0 * scgms::mgdL_2_mmolL },
+	{ 30.0 * scgms::mgdL_2_mmolL,  50.0 * scgms::mgdL_2_mmolL },
+	{ 140.0 * scgms::mgdL_2_mmolL, 170.0 * scgms::mgdL_2_mmolL },
+	{ 280.0 * scgms::mgdL_2_mmolL, 380.0 *scgms::mgdL_2_mmolL },
+	{ 430.0 * scgms::mgdL_2_mmolL, 550.0 *scgms::mgdL_2_mmolL },
+	{ 260.0 * scgms::mgdL_2_mmolL, 550.0 * scgms::mgdL_2_mmolL },
+	{ 70.0 * scgms::mgdL_2_mmolL,  110.0 * scgms::mgdL_2_mmolL },
+	{ 50.0 * scgms::mgdL_2_mmolL,  80.0 * scgms::mgdL_2_mmolL },
+	{ 30.0 * scgms::mgdL_2_mmolL,  60.0 * scgms::mgdL_2_mmolL },
+	{ 0.0 * scgms::mgdL_2_mmolL,   60.0 * scgms::mgdL_2_mmolL }
 };
 
 const std::vector<TError_Grid_Point> PARKES_ZONE_B_Low_T1 = {
-	{ 120.0 *scgms::mgdL_2_mmolL, 0.0 },{ 120.0 *scgms::mgdL_2_mmolL, 30.0 *scgms::mgdL_2_mmolL},{ 260.0 *scgms::mgdL_2_mmolL, 130.0 *scgms::mgdL_2_mmolL},{ 550.0 *scgms::mgdL_2_mmolL, 250.0 *scgms::mgdL_2_mmolL},
-	{ 550.0 *scgms::mgdL_2_mmolL,450.0 *scgms::mgdL_2_mmolL},{ 385.0 * scgms::mgdL_2_mmolL, 300.0 *scgms::mgdL_2_mmolL},{ 170.0 *scgms::mgdL_2_mmolL, 145.0 * scgms::mgdL_2_mmolL },{ 50.0 *scgms::mgdL_2_mmolL, 30.0 *scgms::mgdL_2_mmolL},{ 50.0 *scgms::mgdL_2_mmolL, 0.0 },
+	{ 120.0 * scgms::mgdL_2_mmolL, 0.0 },
+	{ 120.0 * scgms::mgdL_2_mmolL, 30.0 * scgms::mgdL_2_mmolL },
+	{ 260.0 * scgms::mgdL_2_mmolL, 130.0 * scgms::mgdL_2_mmolL },
+	{ 550.0 * scgms::mgdL_2_mmolL, 250.0 * scgms::mgdL_2_mmolL },
+	{ 550.0 * scgms::mgdL_2_mmolL, 450.0 * scgms::mgdL_2_mmolL },
+	{ 385.0 * scgms::mgdL_2_mmolL, 300.0 * scgms::mgdL_2_mmolL },
+	{ 170.0 * scgms::mgdL_2_mmolL, 145.0 * scgms::mgdL_2_mmolL },
+	{ 50.0 * scgms::mgdL_2_mmolL,  30.0 * scgms::mgdL_2_mmolL },
+	{ 50.0 * scgms::mgdL_2_mmolL,  0.0 },
 };
 
 const std::vector<TError_Grid_Point> PARKES_ZONE_C_Up_T1 = {
-	{0.0 ,60.0 *scgms::mgdL_2_mmolL},{ 30.0 *scgms::mgdL_2_mmolL,60.0 *scgms::mgdL_2_mmolL},{ 50.0 *scgms::mgdL_2_mmolL,80.0 *scgms::mgdL_2_mmolL},{ 70.0 *scgms::mgdL_2_mmolL,110.0 *scgms::mgdL_2_mmolL}, { 260.0 *scgms::mgdL_2_mmolL,550.0 *scgms::mgdL_2_mmolL},
-	{ 125.0 * scgms::mgdL_2_mmolL, 550.0 *scgms::mgdL_2_mmolL},{ 80.0 *scgms::mgdL_2_mmolL, 215.0 * scgms::mgdL_2_mmolL },{ 50.0 *scgms::mgdL_2_mmolL, 125.0 * scgms::mgdL_2_mmolL },{ 25.0 * scgms::mgdL_2_mmolL, 100.0 *scgms::mgdL_2_mmolL},{0.0 , 100.0 *scgms::mgdL_2_mmolL},
+	{ 0.0,                         60.0 *scgms::mgdL_2_mmolL },
+	{ 30.0 * scgms::mgdL_2_mmolL,  60.0 *scgms::mgdL_2_mmolL },
+	{ 50.0 * scgms::mgdL_2_mmolL,  80.0 *scgms::mgdL_2_mmolL },
+	{ 70.0 * scgms::mgdL_2_mmolL,  110.0 *scgms::mgdL_2_mmolL },
+	{ 260.0 * scgms::mgdL_2_mmolL, 550.0 *scgms::mgdL_2_mmolL },
+	{ 125.0 * scgms::mgdL_2_mmolL, 550.0 *scgms::mgdL_2_mmolL },
+	{ 80.0 * scgms::mgdL_2_mmolL,  215.0 * scgms::mgdL_2_mmolL },
+	{ 50.0 * scgms::mgdL_2_mmolL,  125.0 * scgms::mgdL_2_mmolL },
+	{ 25.0 * scgms::mgdL_2_mmolL,  100.0 *scgms::mgdL_2_mmolL },
+	{ 0.0,                         100.0 * scgms::mgdL_2_mmolL },
 };
 
 const std::vector<TError_Grid_Point> PARKES_ZONE_C_Low_T1 = {
-	{ 250.0 *scgms::mgdL_2_mmolL, 0.0 },{ 250.0 *scgms::mgdL_2_mmolL, 40.0 *scgms::mgdL_2_mmolL},{ 550.0 *scgms::mgdL_2_mmolL, 150.0 *scgms::mgdL_2_mmolL},
-	{ 550.0 *scgms::mgdL_2_mmolL, 250.0 *scgms::mgdL_2_mmolL},{ 260.0 *scgms::mgdL_2_mmolL, 130.0 *scgms::mgdL_2_mmolL},{ 120.0 *scgms::mgdL_2_mmolL, 30.0 *scgms::mgdL_2_mmolL},{ 120.0 *scgms::mgdL_2_mmolL, 0.0 },
-	
+	{ 250.0 * scgms::mgdL_2_mmolL, 0.0 },
+	{ 250.0 * scgms::mgdL_2_mmolL, 40.0 * scgms::mgdL_2_mmolL },
+	{ 550.0 * scgms::mgdL_2_mmolL, 150.0 * scgms::mgdL_2_mmolL },
+	{ 550.0 * scgms::mgdL_2_mmolL, 250.0 * scgms::mgdL_2_mmolL },
+	{ 260.0 * scgms::mgdL_2_mmolL, 130.0 * scgms::mgdL_2_mmolL },
+	{ 120.0 * scgms::mgdL_2_mmolL, 30.0 * scgms::mgdL_2_mmolL },
+	{ 120.0 * scgms::mgdL_2_mmolL, 0.0 },
 };
 
 const std::vector<TError_Grid_Point> PARKES_ZONE_D_Up_T1 = {
-	{0.0 , 100.0 *scgms::mgdL_2_mmolL},{ 25.0 * scgms::mgdL_2_mmolL, 100.0 *scgms::mgdL_2_mmolL},{ 50.0 *scgms::mgdL_2_mmolL, 125.0 * scgms::mgdL_2_mmolL },{ 80.0 *scgms::mgdL_2_mmolL, 215.0 * scgms::mgdL_2_mmolL },{ 125.0 * scgms::mgdL_2_mmolL, 550.0 *scgms::mgdL_2_mmolL},
-	{ 50.0 *scgms::mgdL_2_mmolL,550.0 *scgms::mgdL_2_mmolL},{ 35.0 * scgms::mgdL_2_mmolL,155.0 * scgms::mgdL_2_mmolL },{0.0 ,150.0 *scgms::mgdL_2_mmolL},
-	
+	{ 0.0,                         100.0 * scgms::mgdL_2_mmolL },
+	{ 25.0 * scgms::mgdL_2_mmolL,  100.0 * scgms::mgdL_2_mmolL },
+	{ 50.0 * scgms::mgdL_2_mmolL,  125.0 * scgms::mgdL_2_mmolL },
+	{ 80.0 * scgms::mgdL_2_mmolL,  215.0 * scgms::mgdL_2_mmolL },
+	{ 125.0 * scgms::mgdL_2_mmolL, 550.0 * scgms::mgdL_2_mmolL },
+	{ 50.0 * scgms::mgdL_2_mmolL,  550.0 * scgms::mgdL_2_mmolL },
+	{ 35.0 * scgms::mgdL_2_mmolL,  155.0 * scgms::mgdL_2_mmolL },
+	{ 0.0,                         150.0 * scgms::mgdL_2_mmolL },
 };
 
 const std::vector<TError_Grid_Point> PARKES_ZONE_D_Low_T1 = {
-	{550.0 *scgms::mgdL_2_mmolL, 0.0},
-	{ 550.0 *scgms::mgdL_2_mmolL, 150.0 *scgms::mgdL_2_mmolL},{ 250.0 *scgms::mgdL_2_mmolL, 40.0 *scgms::mgdL_2_mmolL},	{ 250.0 *scgms::mgdL_2_mmolL, 0.0 },
+	{ 550.0 * scgms::mgdL_2_mmolL, 0.0 },
+	{ 550.0 * scgms::mgdL_2_mmolL, 150.0 *scgms::mgdL_2_mmolL },
+	{ 250.0 * scgms::mgdL_2_mmolL, 40.0 *scgms::mgdL_2_mmolL },
+	{ 250.0 * scgms::mgdL_2_mmolL, 0.0 },
 };
 
 const std::vector<TError_Grid_Point> PARKES_ZONE_E_Up_T1 = {
-	{0.0 ,150.0 *scgms::mgdL_2_mmolL},{ 35.0 * scgms::mgdL_2_mmolL,155.0 * scgms::mgdL_2_mmolL },{ 50.0 *scgms::mgdL_2_mmolL,550.0 *scgms::mgdL_2_mmolL},
-	{0.0 , 550.0 * scgms::mgdL_2_mmolL},
+	{ 0.0,                        150.0 * scgms::mgdL_2_mmolL },
+	{ 35.0 * scgms::mgdL_2_mmolL, 155.0 * scgms::mgdL_2_mmolL },
+	{ 50.0 * scgms::mgdL_2_mmolL, 550.0 * scgms::mgdL_2_mmolL },
+	{ 0.0,                        550.0 * scgms::mgdL_2_mmolL },
 };
+
 #pragma endregion
 
-//Diabetes Type 2 coordinates
-#pragma region DiabetesType2
+// Parkes error grid for Diabetes Type 2
+#pragma region ParkesType2Coords
+
 const std::vector<TError_Grid_Point> PARKES_ZONE_A_T2 = {
-	{ 50.0 *scgms::mgdL_2_mmolL, 0.0 },{ 50.0 *scgms::mgdL_2_mmolL, 30.0 *scgms::mgdL_2_mmolL},{ 90.0 *scgms::mgdL_2_mmolL, 80.0 *scgms::mgdL_2_mmolL},{ 330.0 *scgms::mgdL_2_mmolL, 230.0 *scgms::mgdL_2_mmolL},{ 550.0 *scgms::mgdL_2_mmolL,450.0 *scgms::mgdL_2_mmolL},
-	{ 550.0 *scgms::mgdL_2_mmolL, 550.0 *scgms::mgdL_2_mmolL},
-	{ 440.0 *scgms::mgdL_2_mmolL, 550.0 *scgms::mgdL_2_mmolL},{230.0 *scgms::mgdL_2_mmolL,330.0 * scgms::mgdL_2_mmolL},{ 30.0 *scgms::mgdL_2_mmolL, 50.0 *scgms::mgdL_2_mmolL},{0.0 , 50.0 *scgms::mgdL_2_mmolL},
-	{0.0 , 0.0 },
+	{ 50.0 * scgms::mgdL_2_mmolL,  0.0 },
+	{ 50.0 * scgms::mgdL_2_mmolL,  30.0 * scgms::mgdL_2_mmolL },
+	{ 90.0 * scgms::mgdL_2_mmolL,  80.0 * scgms::mgdL_2_mmolL },
+	{ 330.0 * scgms::mgdL_2_mmolL, 230.0 * scgms::mgdL_2_mmolL },
+	{ 550.0 * scgms::mgdL_2_mmolL, 450.0 * scgms::mgdL_2_mmolL },
+	{ 550.0 * scgms::mgdL_2_mmolL, 550.0 * scgms::mgdL_2_mmolL },
+	{ 440.0 * scgms::mgdL_2_mmolL, 550.0 * scgms::mgdL_2_mmolL },
+	{ 230.0 * scgms::mgdL_2_mmolL, 330.0 * scgms::mgdL_2_mmolL },
+	{ 30.0 * scgms::mgdL_2_mmolL,  50.0 * scgms::mgdL_2_mmolL },
+	{ 0.0,                         50.0 * scgms::mgdL_2_mmolL },
+	{ 0.0,                         0.0 },
 };
 
 const std::vector<TError_Grid_Point> PARKES_ZONE_B_Up_T2 = {
-	{0.0 , 50.0 *scgms::mgdL_2_mmolL},{ 30.0 *scgms::mgdL_2_mmolL, 50.0 *scgms::mgdL_2_mmolL},{ 230.0 *scgms::mgdL_2_mmolL,330.0 *scgms::mgdL_2_mmolL},{ 440.0 *scgms::mgdL_2_mmolL, 550.0 *scgms::mgdL_2_mmolL},
-	{ 280.0 *scgms::mgdL_2_mmolL,550.0 *scgms::mgdL_2_mmolL},{ 30.0 *scgms::mgdL_2_mmolL,60.0 *scgms::mgdL_2_mmolL},{0.0 ,60.0 *scgms::mgdL_2_mmolL}
+	{ 0.0,                         50.0 * scgms::mgdL_2_mmolL },
+	{ 30.0 * scgms::mgdL_2_mmolL,  50.0 *scgms::mgdL_2_mmolL },
+	{ 230.0 * scgms::mgdL_2_mmolL, 330.0 *scgms::mgdL_2_mmolL },
+	{ 440.0 * scgms::mgdL_2_mmolL, 550.0 *scgms::mgdL_2_mmolL },
+	{ 280.0 * scgms::mgdL_2_mmolL, 550.0 *scgms::mgdL_2_mmolL },
+	{ 30.0 * scgms::mgdL_2_mmolL,  60.0 *scgms::mgdL_2_mmolL },
+	{ 0.0,                         60.0 * scgms::mgdL_2_mmolL }
 };
 
 const std::vector<TError_Grid_Point> PARKES_ZONE_B_Low_T2 = {
-	{ 90.0 *scgms::mgdL_2_mmolL, 0.0 },{ 260.0 *scgms::mgdL_2_mmolL, 130.0 *scgms::mgdL_2_mmolL},{ 550.0 *scgms::mgdL_2_mmolL, 250.0 *scgms::mgdL_2_mmolL},
-	{ 550.0 *scgms::mgdL_2_mmolL,450.0 *scgms::mgdL_2_mmolL},{ 330.0 *scgms::mgdL_2_mmolL, 230.0 *scgms::mgdL_2_mmolL},{ 90.0 *scgms::mgdL_2_mmolL, 80.0 *scgms::mgdL_2_mmolL},{ 50.0 *scgms::mgdL_2_mmolL, 30.0 *scgms::mgdL_2_mmolL},{ 50.0 *scgms::mgdL_2_mmolL, 0.0 },
+	{ 90.0 * scgms::mgdL_2_mmolL,  0.0 },
+	{ 260.0 * scgms::mgdL_2_mmolL, 130.0 * scgms::mgdL_2_mmolL },
+	{ 550.0 * scgms::mgdL_2_mmolL, 250.0 * scgms::mgdL_2_mmolL },
+	{ 550.0 * scgms::mgdL_2_mmolL, 450.0 * scgms::mgdL_2_mmolL },
+	{ 330.0 * scgms::mgdL_2_mmolL, 230.0 * scgms::mgdL_2_mmolL },
+	{ 90.0 * scgms::mgdL_2_mmolL,  80.0 * scgms::mgdL_2_mmolL },
+	{ 50.0 * scgms::mgdL_2_mmolL,  30.0 * scgms::mgdL_2_mmolL },
+	{ 50.0 * scgms::mgdL_2_mmolL,  0.0 },
 };
 
 const std::vector<TError_Grid_Point> PARKES_ZONE_C_Up_T2 = {
-	{0.0 ,60.0 *scgms::mgdL_2_mmolL},{ 30.0 *scgms::mgdL_2_mmolL,60.0 *scgms::mgdL_2_mmolL},{ 280.0 *scgms::mgdL_2_mmolL,550.0 *scgms::mgdL_2_mmolL},
-	{ 125.0 * scgms::mgdL_2_mmolL, 550.0 *scgms::mgdL_2_mmolL},{ 35.0 * scgms::mgdL_2_mmolL, 90.0 *scgms::mgdL_2_mmolL},{ 25.0 * scgms::mgdL_2_mmolL, 80.0 *scgms::mgdL_2_mmolL},{0.0 , 80.0 *scgms::mgdL_2_mmolL},
+	{ 0.0,                         60.0 *scgms::mgdL_2_mmolL },
+	{ 30.0 * scgms::mgdL_2_mmolL,  60.0 *scgms::mgdL_2_mmolL },
+	{ 280.0 * scgms::mgdL_2_mmolL, 550.0 *scgms::mgdL_2_mmolL },
+	{ 125.0 * scgms::mgdL_2_mmolL, 550.0 *scgms::mgdL_2_mmolL },
+	{ 35.0 * scgms::mgdL_2_mmolL,  90.0 *scgms::mgdL_2_mmolL },
+	{ 25.0 * scgms::mgdL_2_mmolL,  80.0 *scgms::mgdL_2_mmolL },
+	{ 0.0,                         80.0 *scgms::mgdL_2_mmolL },
 };
 
 const std::vector<TError_Grid_Point> PARKES_ZONE_C_Low_T2 = {
-	{ 250.0 *scgms::mgdL_2_mmolL, 0.0 },{ 250.0 *scgms::mgdL_2_mmolL, 40.0 *scgms::mgdL_2_mmolL},{410.0 *scgms::mgdL_2_mmolL,110.0}, { 550.0 *scgms::mgdL_2_mmolL, 160.0 *scgms::mgdL_2_mmolL},
-	{ 550.0 *scgms::mgdL_2_mmolL, 250.0 *scgms::mgdL_2_mmolL},{ 260.0 *scgms::mgdL_2_mmolL, 130.0 *scgms::mgdL_2_mmolL},{ 90.0 *scgms::mgdL_2_mmolL, 0.0 },
-
+	{ 250.0 * scgms::mgdL_2_mmolL, 0.0 },
+	{ 250.0 * scgms::mgdL_2_mmolL, 40.0 * scgms::mgdL_2_mmolL },
+	{ 410.0 * scgms::mgdL_2_mmolL, 110.0 },
+	{ 550.0 * scgms::mgdL_2_mmolL, 160.0 * scgms::mgdL_2_mmolL },
+	{ 550.0 * scgms::mgdL_2_mmolL, 250.0 * scgms::mgdL_2_mmolL },
+	{ 260.0 * scgms::mgdL_2_mmolL, 130.0 * scgms::mgdL_2_mmolL },
+	{ 90.0 * scgms::mgdL_2_mmolL, 0.0 },
 };
 
 const std::vector<TError_Grid_Point> PARKES_ZONE_D_Up_T2 = {
-	{0.0 , 80.0 *scgms::mgdL_2_mmolL},{ 25.0 * scgms::mgdL_2_mmolL, 80.0 *scgms::mgdL_2_mmolL},{ 35.0 * scgms::mgdL_2_mmolL, 90.0 *scgms::mgdL_2_mmolL},{ 125.0 * scgms::mgdL_2_mmolL, 550.0 *scgms::mgdL_2_mmolL},
-	{ 50.0 *scgms::mgdL_2_mmolL,550.0 *scgms::mgdL_2_mmolL},{ 35.0 * scgms::mgdL_2_mmolL,200.0 *scgms::mgdL_2_mmolL},{0.0 ,200.0 *scgms::mgdL_2_mmolL},
+	{ 0.0,                         80.0 * scgms::mgdL_2_mmolL },
+	{ 25.0 * scgms::mgdL_2_mmolL,  80.0 * scgms::mgdL_2_mmolL },
+	{ 35.0 * scgms::mgdL_2_mmolL,  90.0 * scgms::mgdL_2_mmolL },
+	{ 125.0 * scgms::mgdL_2_mmolL, 550.0 * scgms::mgdL_2_mmolL },
+	{ 50.0 * scgms::mgdL_2_mmolL,  550.0 * scgms::mgdL_2_mmolL },
+	{ 35.0 * scgms::mgdL_2_mmolL,  200.0 * scgms::mgdL_2_mmolL },
+	{ 0.0,                         200.0 * scgms::mgdL_2_mmolL },
 
 };
 
 const std::vector<TError_Grid_Point> PARKES_ZONE_D_Low_T2 = {
-	{ 550.0 *scgms::mgdL_2_mmolL, 160.0 *scgms::mgdL_2_mmolL},{410.0 *scgms::mgdL_2_mmolL,110.0}, { 250.0 *scgms::mgdL_2_mmolL, 40.0 *scgms::mgdL_2_mmolL},{ 250.0 *scgms::mgdL_2_mmolL, 0.0},
-	{ 550.0 *scgms::mgdL_2_mmolL, 0.0 },
+	{ 550.0 * scgms::mgdL_2_mmolL, 160.0 * scgms::mgdL_2_mmolL },
+	{ 410.0 * scgms::mgdL_2_mmolL, 110.0 },
+	{ 250.0 * scgms::mgdL_2_mmolL, 40.0 * scgms::mgdL_2_mmolL },
+	{ 250.0 * scgms::mgdL_2_mmolL, 0.0 },
+	{ 550.0 * scgms::mgdL_2_mmolL, 0.0 },
 };
 
 const std::vector<TError_Grid_Point> PARKES_ZONE_E_Up_T2 = {
-	{0.0,200.0 *scgms::mgdL_2_mmolL},{ 35.0 * scgms::mgdL_2_mmolL,200.0 *scgms::mgdL_2_mmolL},{ 50.0 *scgms::mgdL_2_mmolL,550.0 *scgms::mgdL_2_mmolL},
-	{0.0 , 550.0 *scgms::mgdL_2_mmolL},
+	{ 0.0,                        200.0 * scgms::mgdL_2_mmolL },
+	{ 35.0 * scgms::mgdL_2_mmolL, 200.0 * scgms::mgdL_2_mmolL },
+	{ 50.0 * scgms::mgdL_2_mmolL, 550.0 * scgms::mgdL_2_mmolL },
+	{ 0.0,                        550.0 * scgms::mgdL_2_mmolL },
 };
+
 #pragma endregion
 
 const TError_Grid Parkes_Error_Grid_Type_1 = {
 
-	{ NError_Grid_Zone::A, PARKES_ZONE_A_T1},
+	{ NError_Grid_Zone::A, PARKES_ZONE_A_T1 },
 	
-	{ NError_Grid_Zone::B, PARKES_ZONE_B_Up_T1},
-	{ NError_Grid_Zone::C, PARKES_ZONE_C_Up_T1},
-	{ NError_Grid_Zone::D, PARKES_ZONE_D_Up_T1},
-	{ NError_Grid_Zone::E, PARKES_ZONE_E_Up_T1},
+	{ NError_Grid_Zone::B, PARKES_ZONE_B_Up_T1 },
+	{ NError_Grid_Zone::C, PARKES_ZONE_C_Up_T1 },
+	{ NError_Grid_Zone::D, PARKES_ZONE_D_Up_T1 },
+	{ NError_Grid_Zone::E, PARKES_ZONE_E_Up_T1 },
 
-	{ NError_Grid_Zone::B, PARKES_ZONE_B_Low_T1},
-	{ NError_Grid_Zone::C, PARKES_ZONE_C_Low_T1},
-	{ NError_Grid_Zone::D, PARKES_ZONE_D_Low_T1},
+	{ NError_Grid_Zone::B, PARKES_ZONE_B_Low_T1 },
+	{ NError_Grid_Zone::C, PARKES_ZONE_C_Low_T1 },
+	{ NError_Grid_Zone::D, PARKES_ZONE_D_Low_T1 },
 };
 
 const TError_Grid Parkes_Error_Grid_Type_2 = {
-	{ NError_Grid_Zone::A, PARKES_ZONE_A_T2},
+	{ NError_Grid_Zone::A, PARKES_ZONE_A_T2 },
 
-	{ NError_Grid_Zone::B, PARKES_ZONE_B_Up_T2},
-	{ NError_Grid_Zone::C, PARKES_ZONE_C_Up_T2},
-	{ NError_Grid_Zone::D, PARKES_ZONE_D_Up_T2},
-	{ NError_Grid_Zone::E, PARKES_ZONE_E_Up_T2},
+	{ NError_Grid_Zone::B, PARKES_ZONE_B_Up_T2 },
+	{ NError_Grid_Zone::C, PARKES_ZONE_C_Up_T2 },
+	{ NError_Grid_Zone::D, PARKES_ZONE_D_Up_T2 },
+	{ NError_Grid_Zone::E, PARKES_ZONE_E_Up_T2 },
 
-	{ NError_Grid_Zone::B, PARKES_ZONE_B_Low_T2},
-	{ NError_Grid_Zone::C, PARKES_ZONE_C_Low_T2},
-	{ NError_Grid_Zone::D, PARKES_ZONE_D_Low_T2},
+	{ NError_Grid_Zone::B, PARKES_ZONE_B_Low_T2 },
+	{ NError_Grid_Zone::C, PARKES_ZONE_C_Low_T2 },
+	{ NError_Grid_Zone::D, PARKES_ZONE_D_Low_T2 },
 };
-
