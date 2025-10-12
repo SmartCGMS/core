e58e3a32e1a0a5cde401ca1d0719ec63527566e3
Jonas <jonasx.dufek@gmail.com>
2025-10-13 01:16:49 +0200

    Cannot find eigen3 problem

--- pagmo4scgms/cmake/GetPagmo.cmake
+++ pagmo4scgms/cmake/GetPagmo.cmake
@@ -1,7 +1,6 @@
 include(FetchContent)
 
 message(STATUS "Initalizing the GetPagmo script...")
-
 FetchContent_Declare(
         pagmo
         GIT_REPOSITORY https://github.com/esa/pagmo2.git
@@ -9,6 +8,7 @@ FetchContent_Declare(
         GIT_SHALLOW TRUE
         GIT_PROGRESS TRUE
 )
+
 FetchContent_MakeAvailable(pagmo)
 
 message(STATUS "Pagmo has been initialized in directory: ${pagmo_SOURCE_DIR}")
