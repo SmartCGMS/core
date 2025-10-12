f0c10cae0a3c614ca1f53bc138fb8a6a11b54502
Jonas <jonasx.dufek@gmail.com>
2025-10-13 01:51:42 +0200

    "Build All succeeded"

--- pagmo4scgms/cmake/GetPagmo.cmake
+++ pagmo4scgms/cmake/GetPagmo.cmake
@@ -1,6 +1,6 @@
 include(FetchContent)
-
 message(STATUS "Initalizing the GetPagmo script...")
+
 FetchContent_Declare(
         pagmo
         GIT_REPOSITORY https://github.com/esa/pagmo2.git
@@ -8,7 +8,6 @@ FetchContent_Declare(
         GIT_SHALLOW TRUE
         GIT_PROGRESS TRUE
 )
-
 FetchContent_MakeAvailable(pagmo)
 
 message(STATUS "Pagmo has been initialized in directory: ${pagmo_SOURCE_DIR}")
